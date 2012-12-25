650cfc95a69264404bdf94dec7f916dc4800a6c9
vasilyevd <vasilyev.dmitro@gmail.com>
2012-12-25 10:58:11 +0200

    change header of orgs

--- protected/views/layouts/organization.php
+++ protected/views/layouts/organization.php
@@ -7,33 +7,86 @@ $this->breadcrumbs = array_merge(array(
 
 <?php $this->beginContent('//layouts/main'); ?>
 
+<!-- HEADER FOR ORGANIZATION -->
 <div class="row">
     <div class="span12">
-        <div class="well">
-            <div class="org-header">
-                <div class="row">
-                    <div class="span3">
-                        <a href="<?php echo $this->menu_org->getUploadUrl('logo'); ?>">
-                            <img src="<?php echo $this->menu_org->getUploadUrl('logo'); ?>" alt="Logo">
-                        </a>
-                    </div>
+        <div id="org-header" class="org-header">
 
-                    <div class="span3">
-                        <h3><?php echo CHtml::encode($this->menu_org->name); ?></h3>
-                    </div>
 
-                    <div class="span2 pull-right">
-                        <h3><?php echo CHtml::encode(Lookup::item('OrganizationActionArea',$this->menu_org->action_area)); ?></h3>
-                        <?php if (!empty($this->menu_org->city_id)): ?>
-                            <h3>г. <?php echo CHtml::encode($this->menu_org->city_id); ?></h3>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            </div>
+
+
+	        <!-- NAME -->
+	        <div class="main-name">
+		        <?php echo CHtml::encode($this->menu_org->name); ?>
+	        </div>
+
+
+	        <div class="item-header-container">
+
+		        <!-- FON -->
+		        <div class="item-header-bg">
+		          <?=CHtml::image('/images/orgtitile.jpg'); ?>
+		        </div>
+
+		        <!-- LOGO -->
+		        <div class="item-header-logo" style="">
+			        <img src="<?php echo $this->menu_org->getUploadUrl('logo'); ?>" alt="Logo">
+		        </div>
+
+		        <!-- TEXT -->
+		        <div class="item-header-content">
+			        <p><span class="pre-info">Группа типа: </span> Объединения граждан</p>
+			        <p><span class="pre-info">Тип: </span>Государственное предприятие</p>
+			        <p><span class="pre-info">Область действий: </span>Региональное</p>
+			        <p><span class="pre-info">Направления:</span> </p>
+
+		        </div>
+
+		        <!-- INFO-RIGHT -->
+		        <div class="item-header-widget" style="background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.45), #FFFFFF 60%); margin: 10px; min-height: 170px; padding: 10px 0; position: absolute; right: 0;
+top: 0; width: 160px;">
+
+			        <div class="info" style="font-family: PTSansNarrow; font-size: 16px; font-weight: bold; text-align: center; text-transform: uppercase; margin-bottom: 7px;">
+				        <?php echo CHtml::encode(Lookup::item('OrganizationActionArea',$this->menu_org->action_area)); ?>
+			        </div>
+
+			        <?php if (!empty($this->menu_org->city_id)): ?>
+			        <div class="info" style="font-family: PTSansNarrow; font-size: 16px; font-weight: bold; text-align: center; text-transform: uppercase; margin-bottom: 7px;">
+			        г. <?php echo CHtml::encode($this->menu_org->city_id); ?>
+					    </div>
+			        <?php endif; ?>
+
+		        </div>
+
+
+	        </div>
+
+
+
+	        <!-- MENU -->
+	        <div>
+		        <?php
+		        $this->widget('bootstrap.widgets.TbMenu', array(
+				        'htmlOptions'=>array('class'=>'nav-pills'),
+				        'items'=>array(
+					        array('label'=>'Описание', 'url'=>'#'),
+					        array('label'=>'Новости', 'url'=>'#'),
+					        array('label'=>'Достижения', 'url'=>'#'),
+					        '',
+					        array('label'=>'Галерея', 'url'=>'#'),
+					        array('label'=>'Коллектив', 'url'=>'#'),
+				        ),
+			        ));
+		        ?>
+	        </div>
+
+
+
         </div>
     </div>
 </div>
 
+	<!-- UNDER HEADER - LEFT MENU & CONTENT -->
 <div class="row">
     <div class="span3">
         <?php $this->widget('bootstrap.widgets.TbMenu', array(
