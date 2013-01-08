8e655cb48d8f2119ea4c76d57fb19d1fa4d82e6c
ifsred <atomicclockwork@gmail.com>
2013-01-08 16:29:53 +0200

    New organization search.

--- protected/views/organization/index.php
+++ protected/views/organization/index.php
@@ -1,74 +1,31 @@
 <?php
-$this->sectionMain = "org";
+$this->sectionMain = 'org';
 $this->breadcrumbs=array(
     'Организации',
 );
 
-// $this->menu=array(
-//     array('label'=>'Create Organization','url'=>array('create')),
-//     array('label'=>'Manage Organization','url'=>array('admin')),
-// );
-
 $this->layout='//layouts/column1';
 
 Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-    $('.search-form').toggle();
-    return false;
-});
-$('.search-form form').submit(function(){
-    // This is listview id.
-    $.fn.yiiListView.update('organization-listview', {
-        data: $(this).serialize()
-    });
-    return false;
-});
-// On type listview update.
+// Dynamic form submit and listview update.
 var ajaxUpdateTimeout;
-var ajaxRequest;
-$('input.fast-search').keyup(function(){
-    ajaxRequest = $(this).serialize();
+function dynamicOrganizationFilter() {
     clearTimeout(ajaxUpdateTimeout);
-    ajaxUpdateTimeout = setTimeout(function () {
-        $.fn.yiiListView.update(
-            // This is the id of the CListView.
-            'organization-listview',
-            {data: ajaxRequest}
-        )
-    },
-    // This is the delay.
-    300);
-});
-");
+    ajaxUpdateTimeout = setTimeout(
+        function() {
+            $.fn.yiiListView.update(
+                // This is the id of the CListView.
+                'organization-listview',
+                { data: $('.organization-filter').serialize() }
+            )
+        },
+        // This is the delay.
+        500
+    );
+    return false;
+};
+", CClientScript::POS_END);
 ?>
-
-<div class="search-form">
-    <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-        'action'=>Yii::app()->createUrl($this->route),
-        'method'=>'get',
-    )); ?>
-        <div class="row">
-            <div class="offset3">
-                <div class="form-search">
-                    <div class="input-append">
-                        <?php echo $form->textField($model,'name',array('class'=>'span5 search-query fast-search','maxlength'=>128)); ?>
-                        <?php $this->widget('bootstrap.widgets.TbButton', array(
-                            'buttonType'=>'submit',
-                            'label'=>'Поиск',
-                        )); ?>
-                    </div>
-                </div>
-                <?php echo CHtml::link('Расширенный поиск','#',array('class'=>'search-button')); ?>
-            </div>
-        </div>
-    <?php $this->endWidget(); ?>
-</div>
-
-<div class="search-form" style="display:none">
-    <?php $this->renderPartial('_searchUserFilter',array(
-        'model'=>$model,
-    )); ?>
-</div>
 
 <?php if (!empty($model->type)): ?>
     <p class="lead">Поиск по типу: <?php echo CHtml::encode($model->type->name); ?></p>
@@ -86,16 +43,30 @@ $('input.fast-search').keyup(function(){
     </p>
 <?php endif; ?>
 
-<?php $this->widget('bootstrap.widgets.TbListView',array(
-    'id' => 'organization-listview',
-    'dataProvider' => $model->search(),
-    'itemView' => '_view',
-    // 'viewData' => array('albumId' => $model->id),
-    // 'template' => '{items}{pager}', // Hide summary header.
-    'itemsCssClass' => 'org-search-list', // Items container class. Default: items.
-    'sortableAttributes' => array(
-        'name',
-        'type',
-        'action_area',
-    ),
-)); ?>
+<div class="row">
+    <div class="span9">
+        <?php $this->renderPartial('_searchMainFilter',array(
+            'model'=>$model,
+        )); ?>
+
+        <?php $this->widget('bootstrap.widgets.TbListView',array(
+            'id' => 'organization-listview',
+            'dataProvider' => $model->search(),
+            'itemView' => '_view',
+            // 'viewData' => array('albumId' => $model->id),
+            // 'template' => '{items}{pager}', // Hide summary header.
+            'itemsCssClass' => 'org-search-list', // Items container class. Default: items.
+            'sortableAttributes' => array(
+                'name',
+                'type',
+                'action_area',
+            ),
+        )); ?>
+    </div>
+
+    <div class="span3">
+        <?php $this->renderPartial('_searchSubFilter',array(
+            'model'=>$model,
+        )); ?>
+    </div>
+</div>
