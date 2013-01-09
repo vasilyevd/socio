4c96f1d2ca1067e54db4b1eddfb60ebeb9820a59
ifsred <atomicclockwork@gmail.com>
2013-01-09 13:29:58 +0200

    Calendar finished.

--- protected/views/announcement/index.php
+++ protected/views/announcement/index.php
@@ -2,49 +2,9 @@
 $this->breadcrumbs=array(
     'Лента',
 );
-
-Yii::app()->clientScript->registerScript('search', "
-// Dynamic listview update.
-var ajaxUpdateTimeout;
-function dynamicAnnouncementFilter() {
-    clearTimeout(ajaxUpdateTimeout);
-    ajaxUpdateTimeout = setTimeout(
-        function() {
-            $.fn.yiiListView.update(
-                // This is the id of the CListView.
-                'announcement-listview',
-                { data: $('.announcement-calendar-widget').serialize() }
-            )
-        },
-        // This is the delay.
-        500
-    );
-    return false;
-};
-", CClientScript::POS_END);
 ?>
 
-<?php echo CHtml::link('За всё время', '#', array(
-    'class' => 'announcement-calendar-link',
-    'onclick' => '$(".announcement-calendar-widget").datepicker("show"); return false;',
-)); ?>
-<?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-    'model' => $model,
-    'attribute' => 'publication_time',
-    'language' => 'ru',
-    // jQuery plugin options.
-    'options' => array(
-        'dateFormat' => 'yy-mm-dd',
-        'onSelect' => 'js:function(date) {
-            $(".announcement-calendar-link").text(date);
-            dynamicAnnouncementFilter();
-        },',
-    ),
-    'htmlOptions' => array(
-        'class' => 'announcement-calendar-widget',
-        'style' => 'display:none',
-    ),
-)); ?>
+<?php echo $this->renderPartial('_calendar', array('model' => $model, 'attribute' => 'publication_time')); ?>
 
 <?php $this->widget('bootstrap.widgets.TbListView',array(
     'id' => 'announcement-listview',
